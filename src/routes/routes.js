a6a2e685f6166ff33843b906e5936495ab46b7e9
tjorge30 <mrs.javalera@gmail.com>
2021-04-29 11:52:47 -0600

    set up contact us and nodemailer

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import App from '../App'
 import Login from '../components/Login'
 import Register from '../components/Register'
+import Contact from '../components/Contact'
 import UserLanding from '../components/UserLanding'
 import CreatePost from '../components/CreatePost'
 import Post from '../components/Post'
@@ -15,6 +16,7 @@ export default (
       <Route exact path="/" component={App} />
       <Route path="/login" component={Login} />
       <Route path="/signup" component={Register}/>
+      <Route path="/contact" component={Contact}/>
       <Route path='/user/dash' component={UserLanding} />
       <Route path='/user/createpost' component={CreatePost} />
       <Route path='/user/posts/:id' component={Post} />
