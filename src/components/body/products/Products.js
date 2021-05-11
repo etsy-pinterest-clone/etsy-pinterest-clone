76aacaa6fd5742f2814e5aadd6328420c9862012
Brock Mortenson <mortensonbrock@gmail.com>
2021-05-11 17:10:16 -0600

    cleaned up a couple things and added a back button on the product page.

--- src/components/body/products/Products.js
+++ src/components/body/products/Products.js
@@ -1,21 +1,29 @@
-import React, { useContext, useState, useHistory } from 'react';
+import React, { useContext, useState } from 'react';
 import { DataContext } from '../../../GlobalState';
 import ProductCard from '../../utils/productCard/ProductCard';
 import StoreSearchBar from '../../StoreSearchBar';
+import { useHistory } from 'react-router-dom';
 
 function Products() {
     const state = useContext(DataContext);
     console.log(state)
     const [products] = state.products;
 
-    // const history = useHistory();
+    const history = useHistory();
 
-    
+    function back(e) {
+        e.preventDefault();
+        history.goBack();
+    }
+
     return (
         <div>
-            <h2 className="app_title">
-                    Realtime website ( chat, comments ... ) with MERN Stack and Socket.io
-            </h2>
+            <div>
+                <button onClick={back}>&#8678;</button>
+                <h2 className="app_title">
+                        Realtime website ( chat, comments ... ) with MERN Stack and Socket.io
+                </h2>
+            </div>
             <br />
             <h1>Styling for searchbar and search results is being borrowed from the other search bar and is only being used to help me see the results at the moment</h1>
             <StoreSearchBar/>
