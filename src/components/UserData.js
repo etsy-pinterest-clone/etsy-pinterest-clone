20cbcefc0d38a3c34d0690c1378cf1bcf8830376
tjorge30 <mrs.javalera@gmail.com>
2021-05-12 17:01:41 -0600

    styling

--- src/components/UserData.js
+++ src/components/UserData.js
@@ -23,13 +23,13 @@ const UserData = (props) => {
                 label:'Posts',
                 data:[posts],
                 backgroundColor:[
-                    'rgb(241, 72, 52, 0.7)'
+                    'rgba(182, 182, 182, 0.459)'
                 ],
                 borderWidth:1,
                 borderColor:'rgb(241, 72, 52)',
                 hoverBorderWidth:1,
                 hoverBorderColor:'rgb(121, 38, 163)',
-                hoverBackgroundColor:['rgb(121, 38, 163, 0.7)'],
+                hoverBackgroundColor:['rgb(121, 38, 163, 0.4)'],
                 options:{
                     scales:{
                         yAxesL:[{
@@ -44,13 +44,13 @@ const UserData = (props) => {
                 label:'Posts Saved by Others',
                 data:[savedPosts],
                 backgroundColor:[
-                    'rgb(249, 137, 72, 0.7)'
+                    'rgba(182, 182, 182, 0.459)'
                 ],
                 borderWidth:1,
                 borderColor:'rgb(249, 137, 72)',
                 hoverBorderWidth:1,
                 hoverBorderColor:'rgb(174, 59, 167)',
-                hoverBackgroundColor:['rgb(174, 59, 167, 0.7)'],
+                hoverBackgroundColor:['rgb(174, 59, 167, 0.4)'],
                 options:{
                     scales:{
                         yAxesL:[{
@@ -65,13 +65,13 @@ const UserData = (props) => {
                 label:'Profile Visits',
                 data:[visits],
                 backgroundColor:[
-                    'rgb(254, 186, 85, 0.7)',
+                    'rgba(182, 182, 182, 0.459)',
                 ],
                 borderWidth:1,
                 borderColor:'rgb(254, 186, 85)',
                 hoverBorderWidth:1,
                 hoverBorderColor:'rgb(216, 76, 172)',
-                hoverBackgroundColor:['rgb(216, 76, 172, 0.7)'],
+                hoverBackgroundColor:['rgb(216, 76, 172, 0.4)'],
                 options:{
                     scales:{
                         yAxesL:[{
@@ -115,6 +115,7 @@ const UserData = (props) => {
                     data={chartData}
                     height={200}
                     options={{
+                        indexAxis:'y',
                         title:{
                             display:true,
                             text:'My Data',
