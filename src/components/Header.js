c038cb8e8c4375a85b723f891f98929e2760dc9a
TJared13 <trevin.jared@gmail.com>
2021-06-07 13:33:21 -0700

    responsive landing, login, search

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, {useState} from "react";
 import ReactTooltip from "react-tooltip";
+import {useMediaQuery} from 'react-responsive';
 import { Link, useHistory } from 'react-router-dom';
-import { Button, CircularProgress } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 import { routes } from '../routes/routes';
 import AccountBoxIcon from '@material-ui/icons/AccountBox';
 import EmailIcon from '@material-ui/icons/Email';
@@ -22,6 +23,8 @@ import '../styles/Header.css';
 function Header(props) {
 
     const history = useHistory();
+    const desktop = useMediaQuery({minWidth: 450});
+    const  [view, setView] = useState(false);
             
     const logout = () => {
         try {
@@ -36,30 +39,64 @@ function Header(props) {
         }
     };
 
+    const searchClick = () => {
+        setView(!view)
+    }
+
     return (
-        <div className='header'>
+        <div>
+            { desktop ?
+            <div className='header'>
+                <div className='header__left'>
+                    <div className='homeBtn'>
+                    <Button data-tip data-for="homeTip" onClick={() => history.push('/')}><img className='logo2' src={logo2} alt='logo' /></Button>
+                    <ReactTooltip id='homeTip' place='bottom' effect='solid' className='tooltiptext'>Home Page</ReactTooltip>   
+                    </div>
+                    <Button data-tip data-for="authTip" className='authBtn' onClick={() => history.push("/login")}><AccountBoxIcon/></Button>
+                    <ReactTooltip id='authTip' place='bottom' effect='solid'>Login/ Register</ReactTooltip>
+                    <Button data-tip data-for="contactTip" onClick={() => history.push('/contact')}><EmailIcon/></Button>
+                    <ReactTooltip id='contactTip' place='bottom' effect='solid'>Send us a message</ReactTooltip>
+                    <Link to='/explore' className="exploreLink">Explore</Link>
+                </div>            
+                    <SearchBar />
+                <div className='header__right'>
+                    <Button data-tip data-for="logoutTip" onClick={logout}>
+                        <ExitToAppSharpIcon />
+                    </Button>
+                    <ReactTooltip id='logoutTip' place='bottom' effect='solid'>Logout</ReactTooltip>
+                </div>
+            </div>
+            :
+            <div className='header'>
             <div className='header__left'>
                 <div className='homeBtn'>
                 <Button data-tip data-for="homeTip" onClick={() => history.push('/')}><img className='logo2' src={logo2} alt='logo' /></Button>
                 <ReactTooltip id='homeTip' place='bottom' effect='solid' className='tooltiptext'>Home Page</ReactTooltip>   
                 </div>
+            </div>            
+            <div className='header__right'>
                 <Button data-tip data-for="authTip" className='authBtn' onClick={() => history.push("/login")}><AccountBoxIcon/></Button>
                 <ReactTooltip id='authTip' place='bottom' effect='solid'>Login/ Register</ReactTooltip>
                 <Button data-tip data-for="contactTip" onClick={() => history.push('/contact')}><EmailIcon/></Button>
                 <ReactTooltip id='contactTip' place='bottom' effect='solid'>Send us a message</ReactTooltip>
                 <Link to='/explore' className="exploreLink">Explore</Link>
-            </div>
-            
-
-            <SearchBar />
-
-            <div className='header__right'>
-                <Button data-tip data-for="logoutTip" onClick={logout}>
+                <Button onClick={searchClick}>
+                    <SearchSharpIcon className='search'></SearchSharpIcon>
+                </Button>
+                { view ? 
+                <div className='searchbar' >
+                    <SearchBar />
+                </div>
+                : null
+                }
+                {/* <Button data-tip data-for="logoutTip" onClick={logout}>
                     <ExitToAppSharpIcon />
                 </Button>
-                <ReactTooltip id='logoutTip' place='bottom' effect='solid'>Logout</ReactTooltip>
+                <ReactTooltip id='logoutTip' place='bottom' effect='solid'>Logout</ReactTooltip> */}
             </div>
+        </div>
 
+            }
         </div>
 
     )
